583ba38bfa4692fa1457932f7dd00150bc06427c
@amritmohan <amritmohan2284@gmail.com>
2025-02-11 00:30:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,42 +1,31 @@
 class Rectangle {
-	private _width: number;
-	private _height: number;
-
-	constructor(width: number, height: number) {
-		// Ensure width and height are positive integers
+	constructor(protected width: number, protected height: number) {
 		if (width <= 0 || height <= 0) {
 			throw new Error("Width and height must be positive integers.");
 		}
-		this._width = width;
-		this._height = height;
 	}
 
-	// Getter for width
-	get width(): number {
-		return this._width;
+	getWidth(): number {
+		return this.width;
 	}
 
-	// Getter for height
-	get height(): number {
-		return this._height;
+	getHeight(): number {
+		return this.height;
 	}
 
-	// Method to calculate area
 	getArea(): number {
-		return this._width * this._height;
+		return this.width * this.height;
 	}
 }
 
 class Square extends Rectangle {
 	constructor(side: number) {
-		// Ensure side is a positive integer
+		super(side, side);
 		if (side <= 0) {
 			throw new Error("Side length must be a positive integer.");
 		}
-		super(side, side);
 	}
 
-	// Method to calculate perimeter
 	getPerimeter(): number {
 		return 4 * this.width;
 	}
